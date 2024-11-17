4f53fec5bd1fec65c66d291f41acc9df416c664a
RuiGomes111 <ruisoft01@gmail.com>
2024-11-17 17:15:01 +0100

    gerador de qr code

---
+++ index.js
@@ -0,0 +1,20 @@
+const input= document.querySelector("input")
+const qrcode= document.querySelector("#qrcode")
+
+
+document.addEventListener("keypress", (e)=>{
+    if(e.key==="Enter"){
+        genQRCode()
+    }
+});
+
+
+function genQRCode(){
+    if(!input.value) return
+
+   
+    qrcode.src= `https://api.qrserver.com/v1/create-qr-code/?size=150x150/&data= ${encodeURIComponent (input.value)}`;
+   
+   
+    
+}
